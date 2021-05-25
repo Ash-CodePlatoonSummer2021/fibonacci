726adaeeba33c027e981c7599fd25448e9da3412
asriniv0 <ashsrinivasan1211@gmail.com>
2021-05-25 13:23:21 -0400

    fibonacci

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,14 @@
 const fibonacci = (num) => {
-    
+    function recurse(n){
+      if(n==0){
+        return 0;
+      }
+      if(n==1){
+        return 1;
+      }
+      return recurse(n-1) + recurse(n-2);
+    }
+    return recurse(num);
 }
 
 module.exports = {fibonacci}
